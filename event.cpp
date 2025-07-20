c484615bfbbe7e15f1802d4b173753a2022d4c4b
NEERAJ <145642921+techyneeraj@users.noreply.github.com>
2025-07-20 18:26:05 +0530

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -4,34 +4,36 @@
 #include <sstream>
 #include <iomanip>
 
-Event::Event(const std::string &title, const std::string &date, const std::string &time)
+using namespace std;
+
+Event::Event(const string &title, const string &date, const string &time)
     : title(title), date(date), time(time) {}
 
-std::string Event::getTitle() const { return title; }
-std::string Event::getDate() const { return date; }
-std::string Event::getTime() const { return time; }
+string Event::getTitle() const { return title; }
+string Event::getDate() const { return date; }
+string Event::getTime() const { return time; }
 
-void Event::setTitle(const std::string &t) { title = t; }
-void Event::setDate(const std::string &d) { date = d; }
-void Event::setTime(const std::string &t) { time = t; }
+void Event::setTitle(const string &t) { title = t; }
+void Event::setDate(const string &d) { date = d; }
+void Event::setTime(const string &t) { time = t; }
 
 bool Event::isPast() const {
-    std::tm tm = {};
-    std::istringstream ss(date + " " + time);
-    ss >> std::get_time(&tm, "%Y-%m-%d %H:%M");
-    std::time_t event_time = std::mktime(&tm);
+    tm tm = {};
+    istringstream ss(date + " " + time);
+    ss >> get_time(&tm, "%Y-%m-%d %H:%M");
+    time_t event_time = mktime(&tm);
 
-    std::time_t now = std::time(nullptr);
+    time_t now = time(nullptr);
     return now > event_time;
 }
 
 bool Event::isDueSoon(int minutesThreshold) const {
-    std::tm tm = {};
-    std::istringstream ss(date + " " + time);
-    ss >> std::get_time(&tm, "%Y-%m-%d %H:%M");
-    std::time_t event_time = std::mktime(&tm);
+    tm tm = {};
+    istringstream ss(date + " " + time);
+    ss >> get_time(&tm, "%Y-%m-%d %H:%M");
+    time_t event_time = mktime(&tm);
 
-    std::time_t now = std::time(nullptr);
-    double diff = std::difftime(event_time, now);
+    time_t now = time(nullptr);
+    double diff = difftime(event_time, now);
     return (diff > 0 && diff <= minutesThreshold * 60);
 }
